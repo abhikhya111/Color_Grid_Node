3c0678badec3b4bb5d8115ebecf5fc7e4a3b5da0
Shubham Bhatt <shubham@crackerdigital.com>
2023-04-28 01:59:49 -0700

    port changed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 
 
